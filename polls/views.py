ff26bad910f006f1a618f5da90ac8308afd6e429
Lena <lenakotisova3@yandex.ru>
2022-12-20 19:20:43 +0300

    Add Vote Functinality

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
+from django.urls import reverse
+
 from .models import *
 from django.http import Http404
 
@@ -26,4 +28,16 @@ def results(request, question_id):
 
 
 def vote(request, question_id):
-    return HttpResponse('You are voting for %s' % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except KeyError:
+        return render(request, 'polls/detail.html',
+                      {'question': question,
+                       'error_message': "You didn't select a choice"
+                       })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('results', args=(question.id,)))
