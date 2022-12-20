23d8e51210b0878477f328239e22cd5b841eba89
Лена Котишова <lenakotisova@Lenas-MacBook-Pro.local>
2022-12-20 11:53:27 +0300

    Add Django Admin

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from datetime import datetime
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
 
-# Create your models here.
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return datetime.now() - datetime.timedelta(days=1) <= self.pub_date <= datetime.now()
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
