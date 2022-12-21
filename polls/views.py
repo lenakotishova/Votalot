7e3fd01db06af99c1b3366b26daa9157703e9389
Lena <lenakotisova3@yandex.ru>
2022-12-21 13:11:37 +0300

    Add Generic Views for index, details and results

--- polls/views.py
+++ polls/views.py
@@ -2,34 +2,33 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
+from django.views import generic
 
 from .models import *
 from django.http import Http404
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    return render(request, 'polls/index.html', {
-        'latest_question_list': latest_question_list,
-    }
-                  )
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        # return the latest 5 questions
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404('Question does not exist.')
-    return render(request, 'polls/detail.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
 
-def results(request, question_id):
-    return HttpResponse('This is the results for %s' % question_id)
+class ResultView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except KeyError:
@@ -40,4 +39,4 @@ def vote(request, question_id):
     else:
         selected_choice.votes += 1
         selected_choice.save()
-        return HttpResponseRedirect(reverse('results', args=(question.id,)))
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
