e5267bf0f9930020ee3f0d390285d1ec2d183a49
Lena <lenakotisova3@yandex.ru>
2022-12-31 09:16:06 +0300

    Add Sigh Up

--- polls/views.py
+++ polls/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
-from django.views import generic
+from django.views import generic, View
 from rest_framework.permissions import IsAuthenticated
 
 from .models import *
--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path, include
 from . import views
+from django.views import View
+
+from .views import Register
 
 app_name = 'users'
 
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
+    path('register/', Register.as_view(), name='register'),
 ]
