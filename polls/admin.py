6e4f19ed2bc93e38780d84b627a532109f69d53f
Lena <lenakotisova3@yandex.ru>
2023-01-16 17:58:21 +0300

    Add Comment Section

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Question, Choice
+from .models import Question, Choice, Comment
 
 
 class ChoiceInline(admin.StackedInline):
@@ -19,3 +19,4 @@ class QuestionAdmin(admin.ModelAdmin):
 
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
+admin.site.register(Comment)
--- polls/models.py
+++ polls/models.py
@@ -57,9 +57,9 @@ class Choice(models.Model):
 
 class Comment(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='comments')
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     comment_body = models.TextField(max_length=255)
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return '%s - %s' % (self.question_text, self.author)
+        return '%s - %s' % (self.question.question_text, self.question.author)
